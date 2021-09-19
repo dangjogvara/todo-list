9e2685c1c71214adaef4bcb2a468a7366c335c76
Dan Poulsen <dangjogvara@outlook.com>
2021-09-19 22:55:00 +0100

    documentation

--- app.js
+++ app.js
@@ -10,35 +10,46 @@ tableBody.addEventListener('click', manageTodo);
 // Call this function on load
 document.addEventListener('DOMContentLoaded', loadTable);
 
+// This functions adds To-do's
 function addTodo(e) {
+  // Prevent the page from reloading
   e.preventDefault();
 
+  // Create button element
   const deleteBtn = document.createElement('button');
   deleteBtn.type = 'button';
   deleteBtn.className = 'delete-btn';
   deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
 
+  // Create button element
   const completeBtn = document.createElement('button');
   completeBtn.type = 'button';
   completeBtn.className = 'complete-btn';
   completeBtn.innerHTML = '<i class="fas fa-check"></i>';
 
+  // Create tr element for table
   const row = document.createElement('tr');
   row.className = 'table-row';
 
+  // create td element for table
   const todo = document.createElement('td');
   todo.className = 'todo-text';
 
+  // create td element for table
   const date = document.createElement('td');
   date.className = 'todo-tate';
 
+  // Insert buttons into table elements
   const buttons = document.createElement('td');
   buttons.append(completeBtn, deleteBtn);
 
+  // Get values from inputs
   todo.innerText = todoInput.value;
   date.innerText = dateInput.value;
+  // Append everything to the table
   row.append(todo, date, buttons);
 
+  // Simple alert warning
   if (todoInput.value !== '') {
     tableBody.append(row);
     saveTable(row);
@@ -52,22 +63,25 @@ function addTodo(e) {
 }
 
 function manageTodo(e) {
+  // Check what button you are clicking
   const item = e.target;
   if (item.classList[0] === 'delete-btn') {
     const row = item.parentElement.parentElement;
 
+    // Remove row if button pressed has class delete-btn
     row.remove();
 
+    // Get todos from localStorage
     let todos;
     todos = JSON.parse(localStorage.getItem('todos'));
 
-    console.log(row.firstChild.innerText);
-
+    // Remove todo using filter() that matches the row
     todos = todos.filter(todo => todo.todo !== row.firstChild.innerText);
-    // Save as JSON
+    // Save as JSON in localStorage
     localStorage.setItem('todos', JSON.stringify(todos));
   }
 
+  // Mark task as completed
   if (item.classList[0] === 'complete-btn') {
     const row = item.parentElement.previousSibling.previousSibling;
     row.classList = 'completed';
@@ -77,12 +91,16 @@ function manageTodo(e) {
 // Save data to localStorage
 function saveTable(rows) {
   let todos;
+  // Grab text from inputs
   let todoText = rows.children[0].innerText;
   let todoDate = rows.children[1].innerText;
 
+  // Check if localStorage is empty
   if (localStorage.getItem('todos') === null) {
+    // Init array
     todos = [];
   } else {
+    // Get todos and push unto array
     todos = JSON.parse(localStorage.getItem('todos'));
   }
 
@@ -92,21 +110,26 @@ function saveTable(rows) {
     date: todoDate,
   };
 
+  // Save todo to array
   todos.push(saveTodo);
 
-  // Save as JSON
+  // Save as JSON in localStorage
   localStorage.setItem('todos', JSON.stringify(todos));
 }
 
 // Load Table from localStorage
 function loadTable() {
   let todos;
+  // Check if localStorage is empty
   if (localStorage.getItem('todos') === null) {
+    // Init array
     todos = [];
   } else {
+    // Get todos and push onto array
     todos = JSON.parse(localStorage.getItem('todos'));
   }
 
+  // Loop through each todo in the array and create table (same as in the addTodo() function)
   todos.forEach(function (todo) {
     const deleteBtn = document.createElement('button');
     deleteBtn.type = 'button';
@@ -134,6 +157,7 @@ function loadTable() {
 
     row.append(loadTodo, loadDate, buttons);
 
+    // Append to table
     tableBody.append(row);
   });
 }
