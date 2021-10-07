7160f8a684127a9b65bbdb351dd494471eb12140
Dan Poulsen <dangjogvara@outlook.com>
2021-10-07 20:24:06 +0100

    return last inserted id

--- saveTodo.php
+++ saveTodo.php
@@ -6,6 +6,9 @@ $date = $_POST['date'];
 if (isset($mysqli)) {
     $mysqli->query("INSERT INTO test(todo, date) VALUES('$todo','$date')") or
     die($mysqli->error);
+
+    $last_inserted = $mysqli->insert_id;
+    echo $last_inserted;
 }
 
 
