623c3e76d41e61b7196f46a39e9da37096602bee
Dan Poulsen <dangjogvara@outlook.com>
2021-10-10 15:25:24 +0100

    commenting

--- frontend/canvas.js
+++ frontend/canvas.js
@@ -1,3 +1,4 @@
+// Selectors
 const canvas = document.querySelector('#show-canvas');
 const canvastwo = document.querySelector('#show-canvas-two');
 const ctx = canvas.getContext('2d');
@@ -5,6 +6,7 @@ const ctxtwo = canvastwo.getContext('2d');
 
 
 function startRotating() {
+    // Rotate
     ctx.translate(canvas.width / 2, canvas.height / 2);
     ctxtwo.translate(canvas.width / 2, canvas.height / 2);
     setInterval(rotate, 10);
@@ -12,6 +14,7 @@ function startRotating() {
 
 
 function rotate() {
+    // Rotate properties
     ctx.clearRect(-100, -100, 200, 200);
     ctxtwo.clearRect(-100, -100, 200, 200);
     ctx.rotate(Math.PI / 360);
@@ -22,11 +25,15 @@ function rotate() {
     // The end gradient point is at x=220, y=0
     const gradient = ctx.createLinearGradient(100, 0, 200, 0);
 
-    // Add three color stops
+    // Add colors to gradient
     gradient.addColorStop(0, '#a68b17');
     gradient.addColorStop(.5, '#a73d24');
+
+    // Style
     ctx.fillStyle = gradient;
     ctxtwo.fillStyle = gradient;
+
+    // Draw rectangles with colors
     ctx.fillRect(-50, -50, 100, 100);
     ctxtwo.fillRect(-50, -50, 100, 100);
 
@@ -42,4 +49,5 @@ function rotate() {
     ctxtwo.fillText(`Tasks`, -25, 5);
 }
 
+// init function
 window.addEventListener('load', startRotating);
